7062f7e6850f9beb4d315a6a0dade4cf771481f1
Tyler Canning <tyler@appdetex.com>
2015-11-24 15:30:43 -0700

    Initial commit of empty sample project.

---
+++ src/main/java/com/appdetex/sampleparserjavaproject/Main.java
@@ -0,0 +1,16 @@
+package com.appdetex.sampleparserjavaproject;
+
+/**
+ * Main Java Class
+ *
+ * This class will use Jsoup to retrieve a provided URL
+ * and parse out certain data, printing that data to
+ * stdout in a JSON format.
+ */
+public class Main {
+
+    public static void main( String[] args ) {
+        // Put code here
+    }
+
+}
